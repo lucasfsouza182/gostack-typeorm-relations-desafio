3799c81ec7f93beab4d70a04dec9dd485b2393ef
Lucas Ferreira <lucasfsouza12@gmail.com>
2020-06-02 22:09:33 -0300

    Created route to create a new product

--- src/shared/infra/typeorm/migrations/1591143963528-CreateProduct.ts
+++ src/shared/infra/typeorm/migrations/1591143963528-CreateProduct.ts
@@ -1,10 +1,10 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export default class CreateCustomer1591060557855 implements MigrationInterface {
+export default class CreateProduct1591143963528 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: 'customers',
+        name: 'products',
         columns: [
           {
             name: 'id',
@@ -18,8 +18,14 @@ export default class CreateCustomer1591060557855 implements MigrationInterface {
             type: 'varchar',
           },
           {
-            name: 'email',
-            type: 'varchar',
+            name: 'price',
+            type: 'decimal',
+            precision: 10,
+            scale: 2,
+          },
+          {
+            name: 'quantity',
+            type: 'int',
           },
           {
             name: 'created_at',
@@ -37,6 +43,6 @@ export default class CreateCustomer1591060557855 implements MigrationInterface {
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable('customers');
+    await queryRunner.dropTable('products');
   }
 }
